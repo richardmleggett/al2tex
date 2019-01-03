54517d9916e4f8bde63f9cf9d979aabbbf49d473
Samuel Martin <samuel.martin@earlham.ac.uk>
2019-01-03 14:22:06 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'al2tex'
+project = 'Alvis'
 copyright = '2018, Richard Leggett, Samuel Martin'
 author = 'Richard Leggett, Samuel Martin'
 
@@ -101,7 +101,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'al2texdoc'
+htmlhelp_basename = 'alvisdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -128,7 +128,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'al2tex.tex', 'al2tex Documentation',
+    (master_doc, 'alvis.tex', 'Alvis Documentation',
      'Richard Leggett, Samuel Martin', 'manual'),
 ]
 
@@ -138,7 +138,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'al2tex', 'al2tex Documentation',
+    (master_doc, 'Alvis', 'Alvis Documentation',
      [author], 1)
 ]
 
@@ -149,7 +149,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'al2tex', 'al2tex Documentation',
-     author, 'al2tex', 'One line description of project.',
+    (master_doc, 'Alvis', 'Alvis Documentation',
+     author, 'Alvis', 'One line description of project.',
      'Miscellaneous'),
 ]
